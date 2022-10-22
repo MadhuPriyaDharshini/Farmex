a46b88220efb0c74cdeabdffcca86cd6c9801253
Lokesh <lokeshtrt0108@gmail.com>
2022-10-22 07:32:29 +0530

    22/10/2022

--- controller/farmexcntrlr.js
+++ controller/farmexcntrlr.js
@@ -1,68 +1,69 @@
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
-const User = require('../models/UserDetails');
+const User = require("../models/UserDetails");
 
-const alert =  require('alert');
+const alert = require("alert");
 
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 require("dotenv").config();
 
 const JWT_SECRET = process.env.JWT_TOKEN;
 
-createUser = async function(req,res){
-
-  const {UserName , FirstName , LastName , Email , password} = req.body;
-  
-  const encryptedPassword = await bcrypt.hash(password,10);
-
-  const NewUser = {UserName , FirstName , LastName , Email , password:encryptedPassword};
-
-  console.log(NewUser);
-
-  try{
-    const oldUser = await User.findOne({ Email });
-
-    if(oldUser){
-      return res.send({error:"User exists"});
-    }
-    console.log('called')
-    await User.create(NewUser);
-    alert("Sign Up Successful")
-    res.send('');
-  }catch(error){
-    res.send({status :"error"});
-    console.log(error)
+createUser = async function (req, res) {
+ const { UserName, FirstName, LastName, Email, password } = req.body;
+ const encryptedPassword = await bcrypt.hash(password, 10);
+ const NewUser = {
+    UserName,
+    FirstName,
+    LastName,
+    Email,
+    password: encryptedPassword,
+  };
+  try {
+     const oldUser = await User.findOne({ Email });
+     if (oldUser) {
+        return res.send({ error: "User exists" }); 
+     }
+     await User.create(NewUser);
+     jwt.sign({Email},JWT_SECRET,{expiresIn : '7 days'},(err,token)=>{
+        if(err) throw err;
+        res.json({token});
+        alert("Token Created");
+     })
+  } catch (error) {
+    res.send({ status: "error" });
+    console.log(error);
   }
+};
 
-}
-
-validateUser = async function(req,res){
-  const { Email , password} = req.body;
-  console.log(req.body);
-
+validateUser = async function (req, res) {
+  const { Email, password } = req.body;
   const user = await User.findOne({ Email });
-
-  if(!user){
+  if (!user) {
     alert("User not found please sign up");
     return res.send("");
   }
-
-  if(await bcrypt.compare(password, user.password)){
-    const token = jwt.sign({},JWT_SECRET);
-
-    if(res.status(201)){
-      alert("Login Successful");
-      return res.json({status:"ok", data:token });
-    }else{
-      return res.json({error:"error"});
-    }
-    
+  if (await bcrypt.compare(password, user.password)) {
+    const token = jwt.sign({ Email }, JWT_SECRET,{expiresIn:'30 days'},
+    (err,token)=>{
+        if(err){
+         return res.json({error:"error"});      
+        }
+        alert("Login Successful !!");
+        return res.json({accessToken : token });
+    });
   }
-
-  alert("Invalid Password Please enter the correct one");
-  res.send('/');
-
+  
 }
 
-module.exports={createUser,validateUser};
+UserInfo = async (req,res)=>{
+  try{
+    const user = await User.findOne({UserName}).select('-password');
+    res.status(200).json({user});
+  }catch(error){
+    res.status(500).json(error);
+  }
+}
+
+module.exports = { createUser, validateUser };
