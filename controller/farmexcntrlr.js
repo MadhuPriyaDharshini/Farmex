832fbd160e3cbc4df536d44dc37ec28872a5b3ad
Lokesh <lokeshtrt0108@gmail.com>
2022-10-22 14:18:29 +0530

    22/10/2022

--- controller/farmexcntrlr.js
+++ controller/farmexcntrlr.js
@@ -11,21 +11,25 @@ require("dotenv").config();
 const JWT_SECRET = process.env.JWT_TOKEN;
 
 createUser = async function (req, res) {
- const { UserName, FirstName, LastName, Email, password } = req.body;
- const encryptedPassword = await bcrypt.hash(password, 10);
- const NewUser = {
+  const { UserName, FirstName, LastName, Email, password } = req.body;
+  
+  const encryptedPassword = await bcrypt.hash(password, 10);
+  const NewUser = {
     UserName,
     FirstName,
     LastName,
     Email,
     password: encryptedPassword,
   };
+ 
   try {
-     const oldUser = await User.findOne({ Email });
-     if (oldUser) {
-        return res.send({ error: "User exists" }); 
-     }
-     await User.create(NewUser);
+    const oldUser = await User.findOne({ Email });
+    console.log(oldUser);
+    if (oldUser) {
+      return res.send({ error: "User exists" });
+    }
+    await User.create(NewUser);
+    
      jwt.sign({Email},JWT_SECRET,{expiresIn : '7 days'},(err,token)=>{
         if(err) throw err;
         res.json({token});
@@ -39,31 +43,44 @@ createUser = async function (req, res) {
 
 validateUser = async function (req, res) {
   const { Email, password } = req.body;
+  
   const user = await User.findOne({ Email });
+  console.log(user);
   if (!user) {
     alert("User not found please sign up");
-    return res.send("");
+    return res.json({error : "User not found please sign up"});
   }
-  if (await bcrypt.compare(password, user.password)) {
-    const token = jwt.sign({ Email }, JWT_SECRET,{expiresIn:'30 days'},
-    (err,token)=>{
-        if(err){
-         return res.json({error:"error"});      
-        }
-        alert("Login Successful !!");
-        return res.json({accessToken : token });
-    });
+  const isMatch = await bcrypt.compare(password, user.password);
+  console.log(isMatch);
+  if (!isMatch) {
+    return res.status(400).json({ msg: " Password incorrect" });
   }
-  
-}
 
-UserInfo = async (req,res)=>{
-  try{
-    const user = await User.findOne({UserName}).select('-password');
-    res.status(200).json({user});
-  }catch(error){
+ 
+  const payload = {
+    user: {
+      id: user.id,
+    },
+  };
+
+  jwt.sign(
+    payload,
+    JWT_SECRET,
+    { expiresIn: "30 days" },
+    (err, token) => {
+      if (err) throw err;
+      res.json({ token });
+    }
+  );
+};
+
+UserInfo = async (req, res) => {
+  try {
+    const user = await User.findOne({ UserName }).select("-password");
+    res.status(200).json({ user });
+  } catch (error) {
     res.status(500).json(error);
   }
-}
+};
 
 module.exports = { createUser, validateUser };
